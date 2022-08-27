7d10429f6de59e83d77dedd682a82e4f48302cf7
Kyle Buckner <kylebuckner88@gmail.com>
2022-08-27 11:00:25 -0500

    yipee

--- shreddersskatepark/urls.py
+++ shreddersskatepark/urls.py
@@ -4,13 +4,16 @@ from rest_framework import routers
 from shreddersapi.views.trick_type import TrickTypeView
 from shreddersapi.views.trick import TrickView
 from django.conf.urls import include
+from shreddersapi.views import register_user, login_user
 
-router =  routers.DefaultRouter(trailings_slash=False)
+router =  routers.DefaultRouter(trailing_slash=False)
 router.register(r'tricktypes', TrickTypeView, 'tricktype')
 router.register(r'tricks', TrickView, 'trick')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
+    path('register', register_user),
+    path('login', login_user)
 
 ]
